615937037514f3935522a703aaee2d6f0d9e2812
Bálint Böde <balint.bode@gmail.com>
2014-05-21 16:14:57 +0200

    Add attrs.xml; add layout attribute

--- src/main/java/net/solutinno/android/widget/QuickEditLayout.java
+++ src/main/java/net/solutinno/android/widget/QuickEditLayout.java
@@ -1,5 +1,38 @@
 package net.solutinno.android.widget;
 
-public class QuickEditLayout
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class QuickEditLayout extends ViewGroup
 {
+    private int mLayoutId = -1;
+
+    public QuickEditLayout(Context context) {
+        super(context);
+    }
+
+    public QuickEditLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initAttributes(attrs);
+    }
+
+    public QuickEditLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initAttributes(attrs);
+    }
+
+    private void initAttributes(AttributeSet attrs) {
+        mLayoutId = -1;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (mLayoutId < 0) return;
+        createRootView();
+    }
+
+    private void createRootView() {
+
+    }
 }
